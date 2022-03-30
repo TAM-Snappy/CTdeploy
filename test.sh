dbb86eefe26cf9ffefd9df23e182da4c40a1b298
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-30 16:03:11 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 echo "pct create 100 local:vztmpl/centos-7-default_20190224_amd64.tar.xz --hostname $1 --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=$2/24,gw=192.168.1.201 --nameserver 1.1.1.1 --password $3 --ssh-public-keys \"$4\"" > tmp
 
