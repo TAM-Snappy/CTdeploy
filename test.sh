1d7fef69a6f994c906ace1719f5861edca4b8dc4
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-30 16:45:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "pct create 100 local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname test --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=192.168.1.215/24,gw=192.168.1.201 --nameserver 1.1.1.1 --password password" > tmp
+echo "pct create 100 local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname $var_name --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=$var_ip/24,gw=192.168.1.201 --nameserver 1.1.1.1 --password $var_password" >> tmp
+
 
 ssh root@192.168.1.210 'bash -s' < tmp
-rm -f tmp
+
+
+
