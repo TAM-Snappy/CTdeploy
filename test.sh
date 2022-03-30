ec3220f15c590a6b186d62bd7f8177fb82c7818a
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-30 16:22:47 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "pct create 100 local:vztmpl/centos-7-default_20190224_amd64.tar.xz --hostname test --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=192.168.1.215/24,gw=192.168.1.201 --nameserver 1.1.1.1 --password password" > tmp
+echo "pct create 100 local:vztmpl/debian-11.0-1_amd64.tar.gz --hostname test --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=192.168.1.215/24,gw=192.168.1.201 --nameserver 1.1.1.1 --password password" > tmp
 
 ssh root@192.168.1.210 'bash -s' < tmp
 rm -f tmp
