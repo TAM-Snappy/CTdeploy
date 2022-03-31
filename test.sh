aa5203f1ab5729d19b11dff05b436b515e132fb8
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 11:41:26 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "$5" > sshpubkey
+
+scp sshpubkey root@192.168.1.210:sshpubkey
 scp deploy.sh root@192.168.1.210:deploy.sh
-ssh root@192.168.1.210 "chmod 755 deploy.sh; bash deploy.sh $1 $2 $3 $4 \"$5\"; rm -f deploy.sh"
+ssh root@192.168.1.210 "chmod 755 deploy.sh; bash deploy.sh $1 $2 $3 $4; rm -f deploy.sh"
