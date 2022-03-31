4fbb88d3559965765f37d310fc980a599e50a068
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 12:00:03 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp service.sh root@$1:service.sh
-ssh root@$1 "chmod 755 service.sh; bash service.sh $2; rm -f service.sh"
+scp -o "StrictHostKeyChecking=no" service.sh root@$1:service.sh
+ssh -o "StrictHostKeyChecking=no" root@$1 "chmod 755 service.sh; bash service.sh $2; rm -f service.sh"
