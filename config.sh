6d80a71118910ae8ad76dac3d023e12678353ab0
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 12:13:51 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "En attente du lancement du conteneur .."
+while ! ping -c 1 $1 &> /dev/null; do
+        sleep 1
+done
+
+
 scp -o "StrictHostKeyChecking=no" service.sh root@$1:service.sh
 ssh -o "StrictHostKeyChecking=no" root@$1 "chmod 755 service.sh; bash service.sh $2; rm -f service.sh"
