a6bdbb06e0fa8ca16472b572d3b2dc182df1c06d
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 11:36:39 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 scp deploy.sh root@192.168.1.210:deploy.sh
-ssh root@192.168.1.210 'chmod 755 deploy.sh; bash deploy.sh' 
+ssh root@192.168.1.210 "chmod 755 deploy.sh; bash deploy.sh $1 $2 $3 $4 \"$5\"" 
