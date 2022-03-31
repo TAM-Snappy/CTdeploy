c2e7685f04504f025c2b01f07bf3ad5d09cc89ec
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 12:32:53 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@ while ! ping -c 1 $1; do
         sleep 1
 done
 
+>/var/jenkins_home/.ssh/known_hosts
 
 scp -o "StrictHostKeyChecking=no" service.sh root@$1:service.sh
 ssh -o "StrictHostKeyChecking=no" root@$1 "chmod 755 service.sh; bash service.sh $2; rm -f service.sh"
