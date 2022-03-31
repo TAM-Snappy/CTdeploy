26a0bba096c4eb008a32d8b685b9a68aadbd7f96
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-03-31 11:42:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pct create $4 local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname $1 --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=$3/24,gw=192.168.1.201 --nameserver 1.1.1.1 --start 1 --password $2 --ssh-public-keys "$5"
+pct create $4 local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname $1 --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=$3/24,gw=192.168.1.201 --nameserver 1.1.1.1 --start 1 --password $2 --ssh-public-keys "sshpubkey" 
